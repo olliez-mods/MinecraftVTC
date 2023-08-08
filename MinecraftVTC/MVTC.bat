6b8a37906fd0d4b5f82163245cdf621ba415f051
olliez-mods <75761250+olliez-mods@users.noreply.github.com>
2023-08-08 10:18:30 -0400

    Add files via upload

---
+++ MinecraftVTC/MVTC.bat
@@ -0,0 +1,42 @@
+@echo off
+
+
+::This block of code is provided by stackoverflow:
+::https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+::It requests admin form the user
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+
+:: Here my actual script starts
+cd /D "%~dp0/source"
+python main.py
+pause
